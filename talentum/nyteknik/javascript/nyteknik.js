fd14287977741fd02cf5ea0106bbba2bcd143769
Razi Kantorp <razi.kantorp@whitespace.se>
2015-01-30 13:25:52 +0100

    ...

--- talentum/nyteknik/javascript/nyteknik.js
+++ talentum/nyteknik/javascript/nyteknik.js
@@ -360,7 +360,7 @@ NyTeknik.tabbedBox = function() {
 /*Move company info box in mobile view*/
 NyTeknik.moveJobBox = function() {
     var DOMReady = function() {
-        $("body.page-lediga-jobb.resp .article .quickinfo:nth-child(2)").each(function() {
+        $("body.page-lediga-jobb.resp .quickinfo:nth-child(2)").each(function() {
                                   var boxInfo = $(this);
                                   boxInfo.addClass("test");
                                   
