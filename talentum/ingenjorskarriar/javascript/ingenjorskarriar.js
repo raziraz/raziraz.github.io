918a27c710221af398f1eb5b8be338ec43137e6d
Razi Kantorp <razi.kantorp@whitespace.se>
2015-01-30 10:04:13 +0100

    New mobile menu function

--- talentum/ingenjorskarriar/javascript/ingenjorskarriar.js
+++ talentum/ingenjorskarriar/javascript/ingenjorskarriar.js
@@ -636,3 +636,41 @@ function getEventTarget(evt) {
     }
     return targ;
 }
+
+// Mobile menu
+function hasClass(ele,cls) {
+   	return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+function addClass(ele,cls) {
+	if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(ele,cls) {
+	if (hasClass(ele,cls)) {
+	    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+	   ele.className=ele.className.replace(reg,' ');
+	   }
+	 }
+
+ function init() {
+ 	document.getElementById("toggle").addEventListener("click", toggleMenu);
+ }
+
+ function toggleMenu() {
+ 	var ele = document.getElementsByTagName('body')[0];
+ 		if (hasClass(ele, "open")) {
+ 			addClass(ele, "close");
+ 			removeClass(ele, "open");
+ 		}
+ 		else {
+ 			removeClass(ele, "close");
+ 			addClass(ele, "open");
+ 		}
+ 	}
+
+	document.addEventListener('readystatechange', function() {
+    	if (document.readyState === "complete") {
+       		init();
+    	 }
+   	});
